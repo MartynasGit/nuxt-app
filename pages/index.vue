bb12cfecefe866598c2930aa29f9b03191a7ab4d
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-12-06 17:53:03 +0200

    Added custom styles with tailwind.css. Added ability to fetch and render data from fake store api.

--- pages/index.vue
+++ pages/index.vue
@@ -17,11 +17,4 @@
 <script setup></script>
 
 <style scoped>
-h2 {
-  margin-bottom: 20px;
-  font-size: 36px;
-}
-p {
-  margin: 20px 0;
-}
 </style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-      consequatur? Nam tenetur quod illo quam, repudiandae, voluptatibus ipsa
-      quae quis laudantium deleniti, deserunt unde reiciendis cum hic minus
-      aliquid nihil!
-    </p>
+    <p>{{product.title}}</p>
+    <p>{{product.price}}</p>
+    <p>{{product.id}}</p>
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+// fetch the product
+const { data: product } = await useFetch(uri, {key: id});
 definePageMeta({
   layout: "products",
 });
